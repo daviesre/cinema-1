fd2034c45088d0ce99aad645f99f3e0f49f62da3
shokouh <Shokouh@.com>
2016-08-09 12:04:17 -0700

    add test to save order to database

--- Objects/Order.cs
+++ Objects/Order.cs
@@ -99,6 +99,45 @@ namespace Cinema
       return allorders;
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO orders (showing_id, user_id, quantity) OUTPUT INSERTED.id VALUES (@OrderShowingId, @OrderUserId, @OrderQuantity);", conn);
+
+      SqlParameter showingIdParameter = new SqlParameter();
+      showingIdParameter.ParameterName = "@OrderShowingId";
+      showingIdParameter.Value = this.GetShowingId();
+
+      SqlParameter userIdParameter = new SqlParameter();
+      userIdParameter.ParameterName = "@OrderUserId";
+      userIdParameter.Value = this.GetUserId();
+
+      SqlParameter quantityParameter = new SqlParameter();
+      quantityParameter.ParameterName = "@OrderQuantity";
+      quantityParameter.Value = this.GetQuantity();
+
+      cmd.Parameters.Add(showingIdParameter);
+      cmd.Parameters.Add(userIdParameter);
+      cmd.Parameters.Add(quantityParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
